63cf60cf7e090125ab0559fea6cb760d2126635f
Dawn Chan <dawnchan030920@outlook.com>
2024-05-16 21:53:32 +0800

    [test] add test for BinaryFunctionGroupTests.cs

---
+++ Nymph.Model.Test/BinaryFunctionGroupTests.cs
@@ -0,0 +1,29 @@
+﻿using LanguageExt;
+using Nymph.Model.Group;
+using Nymph.Model.Item;
+using static LanguageExt.Prelude;
+
+namespace Nymph.Model.Test;
+
+public class BinaryFunctionGroupTests
+{
+    [Fact]
+    public async Task GetFinalResult_ReturnsExpectedResult_WhenGivenValidParameters()
+    {
+        var binaryFunctionGroup = new BinaryFunctionGroup<AtomItem<string>, AtomItem<string>, AtomItem<string>>(
+            new FunctionItem<AtomItem<string>, FunctionItem<AtomItem<string>, AtomItem<string>>>(a => 
+                Task.FromResult(
+                    Seq1(new FunctionItem<AtomItem<string>, AtomItem<string>>(b => 
+                    Task.FromResult(
+                        Seq1(
+                        new AtomItem<string>($"{a.Value}{b.Value}")))))
+                    )));
+        var param1 = new AtomItem<string>("Hello");
+        var param2 = new AtomItem<string>("World");
+
+        var result = await binaryFunctionGroup.GetFinalResult(param1, param2).IfNoneAsync(Task.FromResult(LanguageExt.Seq<Item.Item>.Empty));
+
+        Assert.Single(result.Result);
+        Assert.Equal("HelloWorld", ((AtomItem<string>)result.Result.Head).Value);
+    }
+}
