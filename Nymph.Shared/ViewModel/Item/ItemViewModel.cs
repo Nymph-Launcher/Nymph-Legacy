2ee51ae6ac89033ca17d9f3273a7cd747a829497
Dawn Chan <dawnchan030920@outlook.com>
2024-05-18 21:01:05 +0800

    [add] add ItemViewModel.cs

--- Nymph.Shared/ViewModel/Item/ItemViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Nymph.Shared.ViewModel.Item;
-
-public abstract class ItemViewModel
-{
-    public abstract ObservableAsPropertyHelper<Model.Item.Item> Item { get; }
-}
---
+++ Nymph.Shared/ViewModel/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace Nymph.Shared.ViewModel;
+
+public abstract class ItemViewModel : ReactiveObject
+{
+    public abstract Model.Item.Item GetItem { get; }
+}
+
+public class ItemViewModel<T>(T item) : ItemViewModel
+    where T : Model.Item.Item
+{
+    public override Model.Item.Item GetItem => Item;
+    
+    public T Item { get; } = item;
+
+    public ReactiveCommand<Unit, Unit> Choose { get; } = ReactiveCommand.Create(() => { });
+}
