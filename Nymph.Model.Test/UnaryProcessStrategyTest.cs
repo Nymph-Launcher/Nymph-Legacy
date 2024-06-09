9249e3ea9d0b48fc9c3d452e38eee724f2f3af75
xiwen_youmu <1597083201@qq.com>
2024-06-09 23:38:38 +0800

    [test] add test to UnaryProcessStrategyTest

--- Nymph.Model.Test/UnaryProcessStrategyTest.cs
+++ Nymph.Model.Test/UnaryProcessStrategyTest.cs
@@ -15,7 +15,7 @@ public class UnaryProcessStrategyTest
             async param =>
             {
                 await Task.Delay(100);
-                return Seq([new AtomItem<string>("Hello, World!")]);
+                return Seq([new AtomItem<string>("Hello, world!")]);
             });
         var state = new LayerState(
              Seq<Binding>([new Binding("hello", funcItem)]),
@@ -24,6 +24,30 @@ public class UnaryProcessStrategyTest
         var result = strategy.GetGroups(state);
         Assert.NotEmpty(result);
         Assert.All(result, group => Assert.IsAssignableFrom<UnaryFunctionGroup<AtomItem<string>>>(group));
+    }
+
+    [Fact]
+    public void GetGroups_ReturnUnaryFunctionGroupResult_WhenStateItemProper()
+    {
+        var funcItem = new FunctionItem<AtomItem<string>, AtomItem<string>>(
+            async param =>
+            {
+                await Task.Delay(100);
+                return Seq([new AtomItem<string>("Hello, world!")]);
+            });
+        var state = new LayerState(
+            Seq<Binding>([new Binding("hello", funcItem)]),
+            Option<Item.Item>.Some(new AtomItem<string>("Hello, world")), " ");
+        var strategy = new UnaryProcessStrategy();
+        var result = strategy.GetGroups(state);
+        Assert.All(result, async group =>
+        {
+            var funcGroup = group as UnaryFunctionGroup;
+            if (funcGroup == null) return;
+            var atom = await funcGroup.GetResult();
+            var str = atom[0] as AtomItem<string>;
+            Assert.Equal((str ?? new AtomItem<string>("")).GetValue(), "Hello, world!");
+        });
     }
 
     [Fact]
