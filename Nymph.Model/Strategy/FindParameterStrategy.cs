996c66dea587f2edff2546d46c3325f4c228c64f
Dawn Chan <dawnchan030920@outlook.com>
2024-06-10 23:43:05 +0800

    [add] signature for FindParameterStrategy

---
+++ Nymph.Model/Strategy/FindParameterStrategy.cs
@@ -0,0 +1,11 @@
+﻿using LanguageExt;
+
+namespace Nymph.Model.Strategy;
+
+public class FindParameterStrategy : IStrategy
+{
+    public Seq<Group.Group> GetGroups(LayerState state)
+    {
+        throw new NotImplementedException();
+    }
+}
