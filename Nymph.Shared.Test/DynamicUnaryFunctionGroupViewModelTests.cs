827e2623e5e61be0d8ff0e70600827b1c63b7d79
杨夕璃 <w6666s@outlook.com>
2024-06-11 20:56:38 +0800

    [add] dynamicUnaryFunctionGroupViewModel and Tests

---
+++ Nymph.Shared.Test/DynamicUnaryFunctionGroupViewModelTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using DynamicData;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using Nymph.Model.Group;
+using Nymph.Model.Helper;
+using Nymph.Model.Item;
+using Nymph.Shared.ViewModel;
+using Nymph.Shared.ViewModel.GroupViewModel;
+using Nymph.Shared.ViewModel.ItemViewModel;
+using Unit = System.Reactive.Unit;
+
+namespace Nymph.Shared.Test;
+
+using Str = AtomItem<string>;
+
+[TestFixture]
+public class DynamicUnaryFunctionGroupViewModelTests
+{
+    [Test]
+    public void DynamicUnaryFunctionGroupViewModel_ShouldEmitChosenCandidateItemViewModel_WhenChosen()
+    {
+        // Arrange
+        var dynamicUnaryFunctionGroup = new DynamicUnaryFunctionGroup<Str>(
+            new FunctionItem<Str, Str>(
+                async param =>
+                {
+                    await Task.Delay(420);
+                    return Seq(Enumerable.Repeat(param, 10));
+                }
+            ));
+        SourceList<Str> _text= new();
+        var text = _text
+            .Connect()
+            .Cast<Str>();
+        var dynamicUnaryFunctionGroupViewModel =
+            new DynamicUnaryFunctionGroupViewModel<Str>(dynamicUnaryFunctionGroup, text);
+        Item chosen = null;
+        dynamicUnaryFunctionGroupViewModel.ChosenItemViewModels.Subscribe(x => chosen = x);
+        var toBeChosen = dynamicUnaryFunctionGroupViewModel.Items.First();
+        
+        // Act
+        toBeChosen.Choose.Execute().Subscribe();
+        
+        // Assert
+        Assert.That(toBeChosen.ItemViewModel.GetItem, Is.EqualTo(chosen));
+    }
+    
+    [Test]
+    public void DynamicUnaryFunctionGroupViewModel_ShouldBeFunctionItem_WhenFirstIn()
+    {
+        // Arrange
+        var dynamicUnaryFunctionGroup = new DynamicUnaryFunctionGroup<Str>(
+            new FunctionItem<Str, Str>(
+                async param =>
+                {
+                    await Task.Delay(420);
+                    return Seq(Enumerable.Repeat(param, 10));
+                }
+            ));
+        SourceList<Str> _text= new();
+        var text = _text
+            .Connect()
+            .Cast<Str>();
+        var dynamicUnaryFunctionGroupViewModel =
+            new DynamicUnaryFunctionGroupViewModel<Str>(dynamicUnaryFunctionGroup, text);
+        var vm = dynamicUnaryFunctionGroupViewModel.Items.First();
+        
+        // Act
+        
+        // Assert
+        Assert.That(vm.ItemViewModel.GetItem, Is.AssignableFrom(typeof(FunctionItem<Str, Str>)));
+    }
+    
+    [Test]
+    public void StaticUnaryFunctionGroupViewModel_ShouldGetCorrectResults_WhenNextInput()
+    {
+        // Arrange
+        var dynamicUnaryFunctionGroup = new DynamicUnaryFunctionGroup<Str>(
+            new FunctionItem<Str, Str>(
+                async param =>
+                {
+                    await Task.Delay(420);
+                    return Seq(Enumerable.Repeat(param, 10));
+                }
+            ));
+
+        var subject = new Subject<Str>();
+        IObservable<Str> text = subject;
+        
+        var dynamicUnaryFunctionGroupViewModel =
+            new DynamicUnaryFunctionGroupViewModel<Str>(dynamicUnaryFunctionGroup, text);
+        var vm = dynamicUnaryFunctionGroupViewModel.Items.First();
+        // set Auto Execute the Function
+        dynamicUnaryFunctionGroupViewModel.IsAutoExecute = true;
+        var expectedResults = Enumerable.Repeat(new Str("Rx.NET"), 10).ToSeq();
+        
+        // Act
+        subject.OnNext(new Str("NEXT TYPE"));
+        text.Subscribe(_ =>
+        {
+            var results = dynamicUnaryFunctionGroupViewModel.Items
+                .Select(vm => vm.ItemViewModel.GetItem)
+                .Cast<Str>()
+                .ToSeq();
+            
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(results[0], Is.Not.AssignableFrom(typeof(FunctionItem<Str, Str>)));
+                Assert.That(results, Has.Count.EqualTo(10));
+                Assert.That(results.SequenceEqual(expectedResults), Is.True);
+            });
+        });
+    }
+}
--- Nymph.Shared/ViewModel/GroupViewModel/DynamicUnaryFunctionGroupViewModel.cs
+++ Nymph.Shared/ViewModel/GroupViewModel/DynamicUnaryFunctionGroupViewModel.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using DynamicData;
+using LanguageExt;
 using Nymph.Model.Group;
 using Nymph.Model.Item;
 using Nymph.Shared.ViewModel.ItemViewModel;
+using ReactiveUI;
 
 namespace Nymph.Shared.ViewModel.GroupViewModel;
 
 public class DynamicUnaryFunctionGroupViewModel<TResult> : GroupViewModel<DynamicUnaryFunctionGroup<TResult>>
     where TResult : Item
 {
+    public readonly SourceList<CandidateItemViewModel> _candidates = new();
     public override ReadOnlyObservableCollection<CandidateItemViewModel> Items { get; }
     public override IObservable<Item> ChosenItemViewModels { get; }
+    private bool _isAutoExecute;
+
+    public bool IsAutoExecute
+    {
+        get => _isAutoExecute;
+        set => this.RaiseAndSetIfChanged(ref _isAutoExecute, value);
+    }
 
     public DynamicUnaryFunctionGroupViewModel(DynamicUnaryFunctionGroup<TResult> group, IObservable<AtomItem<string>> text) : base(group)
     {
-        
+        _candidates.Add(new CandidateItemViewModel(new ItemViewModelBuilder().Build(group.UnaryFunction)));
+
+        _candidates
+            .Connect()
+            .Bind(out var candidates)
+            .Subscribe();
+        Items = candidates;
+
+        ChosenItemViewModels = _candidates
+            .Connect()
+            .AutoRefresh()
+            .MergeMany(candidate => candidate.Choose.Select(_ => candidate.ItemViewModel))
+            .Select(vm => vm.GetItem)
+            .Publish()
+            .RefCount();
+
+        text
+            .Throttle(TimeSpan.FromMilliseconds(300))
+            .DistinctUntilChanged()
+            .Where(_ => IsAutoExecute)
+            .SelectMany(async t => await group.GetSpecificResult(t))
+            .Select(seq => seq.Select(res => new CandidateItemViewModel(new ItemViewModelBuilder().Build(res))))
+            .Subscribe(seq =>
+            {
+                _candidates.Clear();
+                _candidates.AddRange(seq);
+            });
     }
 }
