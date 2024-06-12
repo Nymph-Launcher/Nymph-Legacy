ff9158c54e81241516ef745e9a89eaccbcc9d783
Dawn Chan <dawnchan030920@outlook.com>
2024-06-13 00:34:56 +0800

    [upd] add ViewModel properties for RecordItemViewModel.cs

--- Nymph.Shared/ViewModel/ItemViewModel/RecordItemViewModel.cs
+++ Nymph.Shared/ViewModel/ItemViewModel/RecordItemViewModel.cs
@@ -1,5 +1,14 @@
-﻿using Nymph.Model.Item;
+﻿using System.Collections.ObjectModel;
+using Nymph.Model.Item;
 
 namespace Nymph.Shared.ViewModel.ItemViewModel;
 
-public class RecordItemViewModel(RecordItem recordItem) : ItemViewModel<RecordItem>(recordItem);
+public class RecordItemViewModel(RecordItem recordItem) : ItemViewModel<RecordItem>(recordItem)
+{
+    public ReadOnlyCollection<Tuple<string, ItemViewModel>> Properties => recordItem
+        .Properties
+        .Select(p => new Tuple<string, ItemViewModel>(p.Item1, new ItemViewModelBuilder()
+            .Build(p.Item2)))
+        .ToList()
+        .AsReadOnly();
+}
