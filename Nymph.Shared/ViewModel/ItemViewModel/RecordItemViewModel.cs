4fc20c1f2336de2b01ee32a70128aa9396d1deb7
Dawn Chan <dawnchan030920@outlook.com>
2024-06-13 00:37:25 +0800

    [hotfix] fix one warning in RecordItemViewModel

--- Nymph.Shared/ViewModel/ItemViewModel/RecordItemViewModel.cs
+++ Nymph.Shared/ViewModel/ItemViewModel/RecordItemViewModel.cs
@@ -5,7 +5,9 @@ namespace Nymph.Shared.ViewModel.ItemViewModel;
 
 public class RecordItemViewModel(RecordItem recordItem) : ItemViewModel<RecordItem>(recordItem)
 {
-    public ReadOnlyCollection<Tuple<string, ItemViewModel>> Properties => recordItem
+    private readonly RecordItem _recordItem = recordItem;
+
+    public ReadOnlyCollection<Tuple<string, ItemViewModel>> Properties => _recordItem
         .Properties
         .Select(p => new Tuple<string, ItemViewModel>(p.Item1, new ItemViewModelBuilder()
             .Build(p.Item2)))
